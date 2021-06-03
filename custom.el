1aadbb6031f181af56732d189513f59eab1598af
Calvin Roth <rothx195@umn.edu>
2021-06-03 11:47:44 -0500

    Add files via upload

---
+++ custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("https://xkcd.com/rss.xml" "https://vox.com/rss/index.xml" "https://www.theatlantic.com/feed/all" "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml" "https://fivethirtyeight.com/all/feed"))
+ '(org-todo-keyword-faces
+   '(("[-]" . +org-todo-active)
+     ("STRT" . +org-todo-active)
+     ("[?]" . +org-todo-onhold)
+     ("WAIT" . +org-todo-onhold)
+     ("HOLD" . +org-todo-onhold)
+     ("PROJ" . +org-todo-project)
+     ("NO" . +org-todo-cancel)
+     ("KILL" . +org-todo-cancel)
+     ("Archived" . "+org-todo-cancel")))
+ '(package-selected-packages '(org-bullets emojify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(+org-todo-other ((t (:inherit (bold font-lock-builtin-face) :foreground "DeepSkyBlue2")))))
